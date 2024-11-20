49ca4d8f2f42c3daad6eb3d0149d731b4739b87f
davidroardev <david_roa_06@hotmail.com>
2024-11-19 19:03:03 -0500

    Funcion crear de CRUD añadida

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import pool from "./dataBase/databaseconnection";
-import { getProducto, getProductoById } from "./controllers/productos_controller";
+import { createProducto, getProducto, getProductoById } from "./controllers/productos_controller";
 
 
 require('dotenv').config();
@@ -9,13 +9,15 @@ const app =  express();
 const port = process.env.PORT
 
 const productosRoutes = Router();
+app.use(express.json())
 
-productosRoutes.get('/getproductos', getProducto)
-productosRoutes.get('/getproductosbyid/:id', getProductoById)
+
+productosRoutes.get('/getproductos', getProducto);
+productosRoutes.get('/getproductosbyid/:id', getProductoById);
+productosRoutes.post('/createproductos', createProducto);
 
 
 app.use(productosRoutes);
-
 app.listen(port, ()=>{
     return console.log(`App listening on port ${port}`)
 });
--- src/controllers/productos_controller.ts
+++ src/controllers/productos_controller.ts
@@ -29,7 +29,36 @@ export const getProductoById = async (req: Request, res: Response): Promise<Resp
         console.error(error);
         return res.status(500).json('Internal Server Error');
     }
-}
+};
 
 
-export
+export const createProducto = async (req: Request, res: Response): Promise<Response> =>{
+    const {id,idProducto,tipoProducto,nombreproducto,tallaProducto,colorProducto, existenciasProducto,estadoProducto}=req.body;
+
+    if (id !== null && idProducto !==null && tipoProducto !== null && nombreproducto !== null && estadoProducto !==null ){
+        try {
+            await pool.query ('INSERT INTO productos (id, id_producto, tipo_producto, nombre, talla,color, existencias, estado) values ($1, $2, $3, $4, $5, $6, $7, $8)',
+                [id,idProducto,tipoProducto,nombreproducto,tallaProducto,colorProducto,existenciasProducto,estadoProducto]
+            );
+            return res.status(201).json({
+                message:'Se creo el producto satisfactoriamente',
+                Producto:{
+                    id,
+                    idProducto,
+                    tipoProducto,
+                    nombreproducto,
+                    tallaProducto,
+                    colorProducto,
+                    existenciasProducto,
+                    estadoProducto
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json('Internal Server Error');
+        }
+    } else{
+        
+        return res.status(500).json('Los datos id, IdProducto, Tipo, Nombre o Estado no pueden estar vacios');
+    }
+};
