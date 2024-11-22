790b0b1279437cc6c279d9f4c2a32f6f66fadb72
davidroardev <david_roa_06@hotmail.com>
2024-11-22 01:54:46 -0500

    Tabla de usuarios añadida y Query para validacion de usuario

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,12 +1,21 @@
-import { Request,Response } from "express";
+import { query, Request,response,Response } from "express";
 import jwt from "jsonwebtoken";
+import pool from "../dataBase/databaseconnection";
 
 require('dotenv').config();
 
-export const generateToken = (req:Request, res:Response): Response=>{
+export const generateToken = async (req:Request, res:Response): Promise<Response>=>{
     const userName = req.body.userName;
-    const user = {name: userName};
-    const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}` , {expiresIn: '1h'});
-    return res.status(200).json({accessToken});
+    const password = req.body.password
+    const query = await pool.query('SELECT * FROM users WHERE user_name = $1 AND password = $2',[userName,password]);
+    console.log(query);
+    console.log(query.rows[0]);
+    const user = query.rows[0];
+    if (query.rowCount !== null && query.rowCount > 0 ){
+        const accessToken = jwt.sign(user, `${process.env.CLAVE_JWT}` , {expiresIn: '1h'});
+        return res.status(200).json({accessToken});
+    } else{
+        return res.status(400).json('El Usuario o la contraseña son Incorrectas');
+    }
 };
 
